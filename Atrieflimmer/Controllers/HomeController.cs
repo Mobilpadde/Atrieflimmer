0b53f2f66a9b08b57f7efb1c33616070983908aa
Mobilpadde <Mobilpadde@gmail.com>
2015-11-27 09:28:02 +0100

    Github to the rescue!
    Yay

--- Atrieflimmer/Controllers/HomeController.cs
+++ Atrieflimmer/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
+using Atrieflimmer.Models;
 
 namespace Atrieflimmer.Controllers
 {
@@ -11,7 +13,18 @@ namespace Atrieflimmer.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.Header = "om";
             return View();
+        }
+
+        public ActionResult Page(string page)
+        {
+            using (SiteEntities ctx = new SiteEntities())
+            {
+                ViewBag.Header = page;
+                ViewBag.Information = ctx.Sites.First(s => s.Name == page).Text;
+                return View();
+            }
         }
     }
 }
