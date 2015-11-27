d6c0085b57ba5b2da73f5f6f7eb6de84471576e2
Mobilpadde <Mobilpadde@gmail.com>
2015-11-27 10:04:27 +0100

    Login :D

---
+++ Atrieflimren/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Atrieflimren.Models;
+
+namespace Atrieflimren.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Header = "om";
+            return View();
+        }
+
+        public ActionResult Page(string page)
+        {
+            using (SiteEntitites ctx = new SiteEntitites())
+            {
+                ViewBag.Header = page;
+                ViewBag.Information = ctx.Sites.First(s => s.Name == page).Text;
+                return View();
+            }
+        }
+    }
+}
