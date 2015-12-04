5aa7bda3202b94dad47e900e88820e6a90bd362f
Mobilpadde <Mobilpadde@gmail.com>
2015-12-04 10:10:58 +0100

    API

---
+++ API2.1/Controllers/UpdateController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Atrieflimren.Models;
+
+namespace API2._1.Controllers
+{
+    public class UpdateController : ApiController
+    {
+        [HttpPut]
+        public bool Update(string path, string text)
+        {
+            using (SiteEntitites ctx = new SiteEntitites())
+            {
+                Site subSite = ctx.Sites.First(s => s.Name ==path);
+                if (subSite != null)
+                {
+                    subSite.Text = text;
+                }
+                ctx.SaveChanges();
+
+                return true;
+            }
+        }
+
+        [HttpGet]
+        public string Get(string path)
+        {
+            using (SiteEntitites ctx = new SiteEntitites())
+            {
+                return ctx.Sites.FirstOrDefault(s => s.Name == path).Text;
+            }
+        }
+    }
+}
---
+++ API2.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="API2._1.WebApiApplication" Language="C#" %>
